80291eb58d523b53f1856e957c474cb9d9cfbc7c
Amanuel <weditedi25@gmail.com>
2021-04-17 14:16:56 +0100

    sending res to the server from your dir

--- server.js
+++ server.js
@@ -3,10 +3,21 @@ const {getStudents} = require("./students");
 const students = getStudents();
 
 const express = require("express");
+const {response} = require("express");
 
 const app = express();
 
 //when server is ready it calls the callback function
 app.listen(3000, function () {
   console.log("Running on port 3000");
+});
+
+app.get("/profiles/eric", (req, res) => {
+  //we are sending a res to the server from the students array
+  const student = students.find((student) => student.username === "eric");
+  res.json(student);
+});
+
+app.get("/profiles/eric/image", (req, res) => {
+  res.sendFile(__dirname + "/images/eric-large.jpg");
 });
