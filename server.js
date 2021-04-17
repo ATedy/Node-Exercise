e1f89070463ffede5b4f90ab00b29ce9b45179c5
Amanuel <weditedi25@gmail.com>
2021-04-17 12:37:15 +0100

    initial commit

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
 const {getStudents} = require("./students");
 
 const students = getStudents();
+
+const express = require("express");
+
+const app = express();
+
+//when server is ready it calls the callback function
+app.listen(3000, function () {
+  console.log("Running on port 3000");
+});
